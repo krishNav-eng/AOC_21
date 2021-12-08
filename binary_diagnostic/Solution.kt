b996e46d7b27de1ff7cc82381fd89ba188abe570
krishNav-eng <71910691+krishNav-eng@users.noreply.github.com>
2021-12-08 13:49:08 +0530

    Update Solution.kt

--- binary_diagnostic/Solution.kt
+++ binary_diagnostic/Solution.kt
@@ -2,53 +2,62 @@ package binary_diagnostic
 
 import java.io.File
 
-fun part_1() {
-    val input = File("/home/abhikrish/IdeaProjects/AOC_21/src/main/kotlin/binary_diagnostic/input.aoc").readLines()
-    var gamma_rate = ""
-    var epsilon_rate = ""
-    for (i in 0 until input[0].length) {
-        var count_1 = 0
-        var count_0 = 0
-        for (j in 0 until input.size) {
-            if(input[j][i] == '1') count_1 ++
-            if(input[j][i] == '0') count_0 ++
-        }
-        if (count_0>count_1) {
-            gamma_rate+="0"
-            epsilon_rate+="1"
-        } else {
-            gamma_rate+="1"
-            epsilon_rate+="0"
+const val InputFilePath = "/binary_diagnostic/input.aoc"
+
+// GammaRate, EpsilonRate
+fun part1(input: List<String>) {
+    val gammaRate: String = buildString {
+        for (i in 0 until input[0].length) {
+            val (zeroes, ones) = input.bitCountInColumn(i)
+            if(zeroes>ones) append('0') else append('1')
         }
     }
-    println(gamma_rate.binToInt()*epsilon_rate.binToInt())
+    val epsilonRate : String = gammaRate.invert()
+    print("GammaRate = $gammaRate, EpsilonRate = $epsilonRate")
+    println(gammaRate.toInt(2)*epsilonRate.toInt(2))
 }
 
-fun part_2() {
-  // Yet, to implement :)
+private fun String.invert(): String = this.asIterable()
+    .joinToString("") { if (it == '0') "1" else "0" }
+
+// O2 generating rating and CO2 scrubber rating
+
+enum class RatingType {
+    OXYGEN,
+    CO2
+}
+
+fun part2(input: List<String>): Int {
+    fun rating(type: RatingType): String {
+        val list: List<String> = input
+        for (c in input[0].indices) {
+            val (zeroes, ones) = list.bitCountInColumn(c)
+            list.filter {
+                when (type) {
+                    RatingType.OXYGEN -> it[c] == if (ones>zeroes) '1' else '0'
+                    RatingType.CO2 -> it[c] != if (ones>zeroes) '1' else '0'
+                }
+            }
+        }
+        return list.single()
+    }
+    return rating(RatingType.OXYGEN).toInt(2) * rating(RatingType.CO2).toInt(2)
+}
+
+fun List<String>.bitCountInColumn(column : Int) : Pair<Int, Int> {
+    var zeroes = 0
+    var ones = 0
+    forEach {
+        when (it[column]) {
+            '0' -> zeroes++
+            '1' -> ones++
+        }
+    }
+    return zeroes to ones
 }
 
 fun main() {
-    part_1()
-    part_2()
-}
-
-// String binary repr to integer
-fun String.binToInt() : Int{
-    var n = 0
-    forEachIndexed { index, c ->
-        n += c.toString().toInt() * 2.pow(length-index-1)
-    }
-    return n
-}
-
-// Simple version of math exponents
-fun Int.pow(i: Int) : Int{
-    if(i==0) return 1
-    if(i>0) {
-        var n = this
-        repeat(i - 1) { n *= this }
-        return n
-    }
-    return -1
+    val input = File(InputFilePath).readLines()
+    part1(input)
+    part2(input)
 }
